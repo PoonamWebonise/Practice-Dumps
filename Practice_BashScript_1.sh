ea44a99a9123e7c0db6a5287fede541b823f75db
PoonamWebonise <poonam.patare@weboniselab.com>
2015-01-30 18:32:09 +0530

    simple commands tried on shell

---
+++ Practice_BashScript_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+
+echo Simple Bash Script - Trying Different Commands
+echo $SHELL
+
+date
+COLOR="blue"
+VALUE="9"
+date +%D
+echo "This is a String: $COLOR"
+echo "Number is: $VALUE"
